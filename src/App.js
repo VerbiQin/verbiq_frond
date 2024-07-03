d86005b2d74241b3a1dae7f4b81132f1878771a4
anfique hussain v <anfiquehussain1@gmail.com>
2024-07-03 17:50:55 +0530

    feat->update(2.0v background particle added)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Comingsoon from './Components/Comingsoon/Comingsoon';
 import AboutUs from './Pages/Legal_Pages/AboutUs/AboutUs';
 import ContactUs from './Pages/Legal_Pages/ContactUs/ContactUs';
 import TermsAndConditions from './Pages/Legal_Pages/Terms And Conditions/TermsAndConditions';
 import PrivacyPolicy from './Pages/Legal_Pages/Privacy Policy/PrivacyPolicy';
 import Disclaimer from './Pages/Legal_Pages/Disclaimer/Disclaimer';
-import CopyrightPolicy from './Pages/Legal_Pages/CopyrightPolicy/CopyrightPolicy';
+import ParticleBackground from './Components/ParticleBackground/ParticleBackground';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data fetching
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 0)); // 2 second delay
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-
     <>
-      <BrowserRouter>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <Navbar />
-            </div>
-          </div>
-          <hr className='hr-nav'></hr>
-          <div className="row">
-            
-            <div className="col">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/comingsoon/:pagename" element={<Comingsoon />} />
-                <Route path="/about_us" element={<AboutUs/>} />
-                <Route path="/contact_us" element={<ContactUs/>} />
-                <Route path="/terms_and_conditions" element={<TermsAndConditions/>} />
-                <Route path="/privacy_policy" element={<PrivacyPolicy/>} />
-                <Route path="/disclaimer" element={<Disclaimer/>} />
-                {/* <Route path="/copyright_policy" element={<CopyrightPolicy/>} /> */}
-
-
-              </Routes>
-            </div>
-          </div>
+      <ParticleBackground /> {/* Add the ParticleBackground component */}
+      {isLoading ? (
+        <div className="loading-container">
+          <div className="loading-spinner"></div>
+          <p>Loading...</p>
         </div>
-      </BrowserRouter>
+      ) : (
+        <BrowserRouter>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <Navbar />
+              </div>
+            </div>
+            <hr className='hr-nav'></hr>
+            <div className="row">
+              <div className="col">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/comingsoon/:pagename" element={<Comingsoon />} />
+                  <Route path="/about_us" element={<AboutUs/>} />
+                  <Route path="/contact_us" element={<ContactUs/>} />
+                  <Route path="/terms_and_conditions" element={<TermsAndConditions/>} />
+                  <Route path="/privacy_policy" element={<PrivacyPolicy/>} />
+                  <Route path="/disclaimer" element={<Disclaimer/>} />
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
+      )}
     </>
   )
 }
